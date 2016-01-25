4f246ee4d74e60bab8c4b5c5f8a667c991ff7024
fomonster <fomonster@gmail.com>
2016-01-25 11:43:31 +0300

    Frist

---
+++ clear.bat
@@ -0,0 +1,7 @@
+del /F /Q /S DragonBones\DragonBones.sdf
+del /F /Q /S DragonBones\Debug\*.lib
+del /F /Q /S DragonBones\Debug\*.pdb
+del /F /Q /S DragonBones\Debug\*.exe
+del /F /Q /S DragonBones\Debug\*.ilk
+del /F /Q /S DragonBones\DragonBones\Debug\*.*
+del /F /Q /S DragonBones\DragonBonesExample\Debug\*.*
